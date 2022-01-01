529465effbc1d9c632438285fac8c0f8702f0734
Onur <onurcanyavas.icloud.com>
2022-01-01 19:31:59 +0300

    commit

--- main.js
+++ main.js
@@ -1,19 +1,6 @@
-const { encrypt, decrypt} = require("./aes");
+const { encrypt, decrypt} = require("./des");
 
 let encrypted = encrypt("oo", "envercan")
 console.log(encrypted);
 let decrypted = decrypt(encrypted, "envercan")
 console.log(decrypted);
-
-
-
-// let encryptedString = ""
-// for (let i=0; i<encrypted.length/8; i++){
-//     let char = encrypted.substring(i*8,i*8+8)
-//     encryptedString += String.fromCharCode(parseInt(char,2))
-// }
-// console.log(encryptedString);
-
-
-// console.log(binStringToHexString(encrypted));
-
