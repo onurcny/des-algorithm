ed353920eb339d8320d8ac14ceb301599018dd45
Onur <onurcanyavas.icloud.com>
2022-01-01 19:49:16 +0300

    commit

--- old/main.js
+++
@@ -1,9 +0,0 @@
-const { User } = require("./user")
-
-let onur = new User("Onur")
-let enver = new User("Enver")
-
-onur.sendFriendRequest(enver)
-
-onur.sendMessage(enver, "selam kanks")
-enver.sendMessage(onur, "sana da selam eyy kanks")
